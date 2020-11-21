502eb3015a18d3783b224b51136402d7b72ed9d7
Onur Üstünel <ustunel.onur@gmail.com>
2020-11-21 20:02:24 +0300

    Initial Commit

---
+++ Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by onur on 21.11.2020.
+//  Copyright © 2020 onurustunel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
